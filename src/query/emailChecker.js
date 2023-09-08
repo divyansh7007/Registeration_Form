cf234ea5931aa71d2416e17a158eae1415df0faa
divyansh7007 <119783951+divyansh7007@users.noreply.github.com>
2023-09-08 15:07:22 +0530

    Delete src/query directory

--- src/query/emailChecker.js
+++
@@ -1,9 +0,0 @@
-
-
-const emailChecker = async (req, res, email) => {
-    // emailPattern 
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    console.log(emailPattern.test(email));
-}
-
-export default emailChecker;
